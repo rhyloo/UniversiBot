8adf16e5fcfc8c45e92a5ed47ea90df2e0c0a4b7
Rhyloo <jorge@uma.es>
2020-11-13 21:06:57 +0100

    At this point the bot works!

--- bot.py
+++ bot.py
@@ -1,47 +1,59 @@
+##token import 
+from data import token
+
+##To use the easy api of telegram
 import telebot
 from telebot.types import InlineKeyboardButton, InlineKeyboardButton
-import time;
+
+#Get the current time
+import time
+
+##"Multitasks"
 import threading
 
 
 ##VARIABLES
-bot_name = "My first agenda"
-bot = telebot.TeleBot("1370488161:AAE4JEvyoz1hVS7KznyU2W-M2UaSpilEOUs")
-init_message = f"""Mi primera ENCARTA"""
+bot_name = "J.A.R.V.I.Sl"
+bot = telebot.TeleBot(token)
+init_message = f"""¡Bienvenido extraño!\nEsta es una agenda privada si
+crees que te lo mereces ingresa la contraseña y observa las tareas de Rhyloo."""
 
 ##FUNCTIONS
 
 
 ### Hilo 1
 def checkit():
-  threading.Timer(5.0, checkit).start()
-  print("actualizado...")
+  #Time default 60.0
+  threading.Timer(60.0, checkit).start()
+  print("Revisado...")
   localtime = time.localtime(time.time())
 
-  if (localtime.tm_hour < 9 ):
+  if (localtime.tm_hour < 10 ):
         localtime_useful_hour = "0"+str(localtime.tm_hour)
   else:
        localtime_useful_hour = str(localtime.tm_hour)
 
-  if (localtime.tm_min < 9 ):
+  if (localtime.tm_min < 10 ):
         localtime_useful_min = "0"+str(localtime.tm_min)
   else:
        localtime_useful_min = str(localtime.tm_min)
 
   localtime_useful = localtime_useful_hour + ":" + localtime_useful_min
+
   agenda = open("notes.org", "r")
   content = agenda.readlines()
   agenda.close()
+
   for i in range(len(content)):
       content_splited =  content[i].split()
+#      print("El número es:",i)
+ #     print("El contenido de content_splited[0] es:", content_splited)
       if (content_splited[0] == ":ALARM:" ):
           content_splited[3] = content_splited[3].replace(']','')
+  #        print("El contenido de content_splited[3] es:",content_splited[3])
           if (content_splited[3] == localtime_useful):
-              send_message("FUNCIONA!")
-             # bot.send_message(message.chat.id,"FUNCIONA!")
-              
+            send_message(content[i-1])
 checkit()
-
 ### Hilo 2
 def send_message(text):
     ids = open('ids.txt', 'r')
@@ -50,9 +62,17 @@ def send_message(text):
     ids.close()
     
 def save_data(chat_id):
-    ids = open('ids.txt', 'a')
-    ids.write(str(chat_id)+'\n')
+    text = str(chat_id)+'\n'
+    write = 0
+    ids = open('ids.txt', 'r')
+    for line in ids:
+      if (line == text):
+        write = write + 1
     ids.close()
+    if (write == 0):
+      ids = open('ids.txt', 'a')
+      ids.write(str(chat_id)+'\n')
+      ids.close()
 
 #is the file?
 def itfile(content):
@@ -131,6 +151,11 @@ def start(message):
 @bot.message_handler(commands=['tareas'])
 def tareas(message):
    task_message = main_todo()
+   bot.send_message(message.chat.id, task_message)
+
+@bot.message_handler(commands=['tareas_hechas'])
+def tareas(message):
+   task_message = main_done()
    bot.send_message(message.chat.id, task_message)
 
 @bot.message_handler(commands=['tareas_hechas'])
